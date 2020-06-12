974f9c58db1acf3e79dc37f391fcf09502789869
RenegadeStudent <47337247+RenegadeStudent@users.noreply.github.com>
2020-06-12 14:07:23 -0700

    Create main.cpp
    This is exactly what I had submitted for my assignment when I took the class. I hope to make a program later that will automatically enter in the borders so that it doesn't have to be hard codded (which looks very messy). Look forward to clean ups in the future.

---
+++ main.cpp
@@ -0,0 +1,30 @@
+//GreetingCardGenerator, RenegadeStudent
+//The quote used was from Oyasumi Punpun by Inio Asano.
+
+#include <iostream>
+#include "stdio.h"
+using namespace std;
+
+int main(){
+    printf("\a");
+    cout << endl<< endl<< "\t\t     Oyasumi Punpun by Inio Asano"<< endl
+         << "\t\t        Harumi Shuntarou said:"<< endl<< endl<< endl;
+
+    cout << "\t\xC9\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD"
+         << "\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD"
+         << "\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD"
+         << "\xCD\xCD\xBB"<< endl
+         << "\t\xBA   People change. And as the years go by, so do \xBA"<< endl
+         << "\t\xBA so do our environments. I think it\'s alright   \xBA"<< endl
+         << "\t\xBA for there not to be a \"proper\" way of living.  \xBA"<< endl
+         << "\t\xBA But I do believe that instead of trying to be  \xBA"<< endl
+         << "\t\xBA satisfied on just one answer and blocking out  \xBA"<< endl
+         << "\t\xBA the rest, searching for more answers, despite  \xBA"<< endl
+         << "\t\xBA the pain they may bring, is a much more honest \xBA"<< endl
+         << "\t\xBA Away of living.                                \xBA"<< endl
+         << "\t\xC9\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD"
+         << "\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD"
+         << "\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD"
+         << "\xCD\xCD\xBB"<< endl<< endl;
+    return 0;
+}
